83b6168e99895aa9d83374250eac44bda37e67d9
Netra <netrahindocha1@gmail.com>
2024-06-14 10:07:42 -0400

    Adding Class, Properties, Constructor and Methods

---
+++ Assignment2/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Product
+    {
+        // Defining Properties
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public int Stock { get; set; }
+
+        // Parameterless Constructor
+        public Product(int productId, string productName, double price, int stock)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        // Method to Increase the Stock
+        public void IncreaseStock(int numOfProducts)
+        {
+            if (numOfProducts > 0)
+            {
+                Stock += numOfProducts;
+            }
+            else 
+            {
+                Stock += 0;
+            }
+        }
+
+        // Method to Decrease the Stock
+        public void DecreaseStock(int numOfProducts) 
+        {
+            if (numOfProducts > 0 && numOfProducts <= Stock)
+            {
+                Stock -= numOfProducts;
+            }
+            else 
+            {
+                Stock -= 0;
+            }
+        }
+
+    }
+}
